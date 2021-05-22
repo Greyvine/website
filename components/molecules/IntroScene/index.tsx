6a6fb3dc406ac6d425b8a618f6359380fc1548fc
GUTTERTRASH <shagohad@gmail.com>
2021-05-21 22:49:07 -0400

    feat(IntroScene): Add controls

--- components/molecules/IntroScene/index.tsx
+++ components/molecules/IntroScene/index.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import { Canvas, MeshProps } from "@react-three/fiber"
+import React, { useEffect, useRef, useState } from "react"
+import { Canvas, MeshProps, useFrame } from "@react-three/fiber"
+import * as THREE from "three"
 
 /*
  *
@@ -53,6 +54,63 @@ import { Canvas, MeshProps } from "@react-three/fiber"
  *     )
  *   }
  */
+
+const Button = () => {
+
+    const [
+        active,
+        setActive
+    ] = useState(false)
+    const vec = new THREE.Vector3()
+    const [
+        zoom,
+        set
+    ] = useState(true)
+    const light = useRef()
+    useEffect(() => {
+
+        document.body.style.cursor = active
+            ? "pointer"
+            : "auto"
+
+    }, [active])
+    useFrame((state) => {
+
+        const step = 0.1
+        state.camera.fov = THREE.MathUtils.lerp(state.camera.fov, zoom
+            ? 10
+            : 42, step)
+        state.camera.position.lerp(vec.set(zoom
+            ? 25
+            : 10, zoom
+            ? 1
+            : 5, zoom
+            ? 0
+            : 10), step)
+        state.camera.lookAt(0, 0, 0)
+        state.camera.updateProjectionMatrix()
+        light.current.position.lerp(vec.set(zoom
+            ? 4
+            : 0, zoom
+            ? 3
+            : 8, zoom
+            ? 3
+            : 5), step)
+
+    })
+    return <mesh receiveShadow castShadow onClick={() => set(!zoom)} onPointerOver={() => setActive(true)} onPointerOut={() => setActive(false)}>
+        <sphereBufferGeometry args={[
+            0.75,
+            64,
+            64
+        ]} />
+        <meshPhysicalMaterial color={active
+            ? "purple"
+            : "#e7b056"} clearcoat={1} clearcoatRoughness={0} />
+        <directionalLight ref={light} castShadow intensity={1.5} shadow-camera-far={70} />
+    </mesh>
+
+}
 
 type PlaneProps = {
     color: string
@@ -137,6 +195,7 @@ const IntroScene: React.FC = () => <Canvas shadows={true} camera={{ position: [
             4
         ]} />
     </group>
+    <Button />
 </Canvas>
 
 export default IntroScene
